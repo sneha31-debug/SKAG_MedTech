30bbc6584ce6394717c5c2eb0a09863a10baba70
krishx06 <krishpatil120@gmail.com>
2026-02-01 08:32:34 +0530

    backend config

--- backend/core/config.py
+++ backend/core/config.py
@@ -70,11 +70,9 @@ class Config:
     
     # CORS
     CORS_ORIGINS: list = [
-        "http://localhost:5173", 
-        "http://localhost:3000", 
-        "http://127.0.0.1:5173",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
+        "https://skag-med-tech.vercel.app"
         "*"  # Allow all origins for development
     ]
     
